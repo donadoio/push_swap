70f68e14f71726d1f82e9cb706acf773b6bee783
Isaac Donado <idonado@pop-os.localdomain>
2021-08-16 19:28:32 +0200

    linked list practice

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -1,11 +1,14 @@
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
+# include <stdlib.h>
 
 typedef	struct 	s_stack
 {
-	void			*value;
+	int				value;
 	struct s_stack	*next;
 }				t_stack;
+
+t_stack	*stack_new(int	number);
 
 
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -1,6 +1,53 @@
 #include <push_swap.h>
 #include <libft.h>
-#include <stdlib.h>
+
+t_stack	*stack_new(int	number)
+{
+	t_stack	*new;
+
+	new = malloc(sizeof(t_stack));
+	new->value = number;
+	new->next = NULL;
+	return (new);
+}
+
+t_stack	*stack_last(t_stack *stack)
+{
+	while (stack->next != NULL)
+		stack = stack->next;
+	return (stack);
+}
+
+void	stack_add_front(t_stack **list, t_stack *new)
+{
+	new->next = *list;
+	return ;
+}
+
+void	stack_add_back(t_stack	**list, t_stack *new)
+{
+	t_stack *last;
+
+	last = stack_last(*list);
+	last->next = new;
+	last->next->next = NULL;
+	return ;
+}
+
+int		stack_size(t_stack	*stack)
+{
+	int i;
+
+	i = 0;
+	if (stack != NULL)
+		i++;
+	while (stack->next != NULL)
+	{
+		i++;
+		stack = stack->next;
+	}
+	return (i);
+}
 
 int	main(int av, char **argc)
 {
@@ -10,21 +57,33 @@ int	main(int av, char **argc)
 	ft_putchar_fd('\n', 1);
 
 	t_stack	*a;
-	a = malloc(sizeof(t_stack));
-	a->value = malloc(sizeof(int));
-	a->value = (void*)42;
-
 	t_stack	*b;
-	b = malloc(sizeof(t_stack));
-	b->value = malloc(sizeof(int));
-	b->value = (void*)43;
 
-	a->next = b;
+	a = stack_new(42);
+	b = stack_new(43);
 
-	ft_putnbr_fd((int)a->value, 1);
-	ft_putnbr_fd((int)a->next->value, 1);
+	stack_add_front(&a, b);
+	ft_putstr_fd("Value of second: ", 1);
+	ft_putnbr_fd(b->next->value, 1);
+	ft_putchar_fd('\n', 1);
+	ft_putstr_fd("Stack size: ", 1);
+	ft_putnbr_fd(stack_size(b), 1);
+	ft_putchar_fd('\n', 1);
+
+	t_stack *c;
+	c = stack_new(55);
+	stack_add_back(&b, c);
+	ft_putstr_fd("Stack size: ", 1);
+	ft_putnbr_fd(stack_size(b), 1);
+	ft_putchar_fd('\n', 1);
+
+	t_stack *last = stack_last(b);
+	ft_putstr_fd("Value of last: ", 1);
+	ft_putnbr_fd(last->value, 1);
+	ft_putchar_fd('\n', 1);
 
 	free(b);
 	free(a);
+	free(c);
 	return (0);
 }
